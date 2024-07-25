980ecd35a90073c15dd6938720f53f5c8f5c2c0f
Richard Lopes <richard.lopes92@gmail.com>
2024-07-24 23:07:32 -0300

    chore: remove unused deps

--- fraql.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "fraql" {
-  import { DocumentNode } from "graphql";
-  export default function gql<T = any>(strings: TemplateStringsArray, ...values: any[]): DocumentNode;
-  function toInlineFragment<T>(fragment: T): T;
-}
